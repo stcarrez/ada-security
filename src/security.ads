8bc07d0361b4f12224e496a885618774fe681df2
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-11-17 16:45:11 +0000

    Make the Principal be a real principal (ie, no knowledge of roles)

--- src/security.ads
+++ src/security.ads
@@ -35,10 +35,6 @@ package Security is
    --  ------------------------------
    type Principal is limited interface;
    type Principal_Access is access all Principal'Class;
---
---     --  Returns true if the given role is stored in the user principal.
---     function Has_Role (User : in Principal;
---                        Role : in Role_Type) return Boolean is abstract;
 
    --  Get the principal name.
    function Get_Name (From : in Principal) return String is abstract;
