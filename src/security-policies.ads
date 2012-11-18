4445bdfe5d8dafd4de86516d1366321d0c5a512a
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-11-18 18:23:20 +0000

    Cleanup the definition

--- src/security-policies.ads
+++ src/security-policies.ads
@@ -30,33 +30,20 @@ with Util.Serialize.IO.XML;
 
 with GNAT.Regexp;
 
+with Security.Permissions;
 limited with Security.Controllers;
 limited with Security.Contexts;
 
---  == Permissions ==
---  The <b>Security.Permissions</b> package defines the different permissions that can be
---  checked by the access control manager.
---
---  === Principal ===
---  The <tt>Principal</tt> is the entity that can be authenticated.  A principal is obtained
---  after successful authentication of a user or of a system through an authorization process.
---  The OpenID or OAuth authentication processes generate such security principal.
---
---  === Permission ===
---  The <tt>Permission</tt> represents an access to a system or application resource.
---  A permission is checked by using the security manager.  The security manager uses a
---  security controller to enforce the permission.
---
 package Security.Policies is
 
    type Policy is new Ada.Finalization.Limited_Controlled with null record;
    type Policy_Access is access all Policy'Class;
 
-   procedure Set_Reader_Config (Pol     : in Policy;
+   procedure Set_Reader_Config (Pol     : in out Policy;
                                 Reader  : in out Util.Serialize.IO.XML.Parser) is null;
 
    --  Get the policy name.
-   function Get_Name (From : in Policy) return String;;
+   function Get_Name (From : in Policy) return String;
 
    Invalid_Name : exception;
 
@@ -64,7 +51,7 @@ package Security.Policies is
 
    type Controller_Access is access all Security.Controllers.Controller'Class;
 
-   type Controller_Access_Array is array (Permission_Index range <>) of Controller_Access;
+   type Controller_Access_Array is array (Permissions.Permission_Index range <>) of Controller_Access;
 
    --  Each permission is represented by a <b>Permission_Type</b> number to provide a fast
    --  and efficient permission check.
@@ -93,33 +80,11 @@ package Security.Policies is
                              Name       : in String;
                              Permission : in Controller_Access);
 
-   --  Returns True if the user has the permission to access the given URI permission.
---     function Has_Permission (Manager    : in Policy_Manager;
---                              Context    : in Security_Context_Access;
---                              Permission : in URI_Permission'Class) return Boolean;
-
-   --  Returns True if the user has the given role permission.
---     function Has_Permission (Manager    : in Permission_Manager;
---                              User       : in Principal'Class;
---                              Permission : in Permission_Type) return Boolean;
-
    --  Get the security controller associated with the permission index <b>Index</b>.
    --  Returns null if there is no such controller.
    function Get_Controller (Manager : in Policy_Manager'Class;
-                            Index   : in Permission_Index) return Controller_Access;
+                            Index   : in Permissions.Permission_Index) return Controller_Access;
    pragma Inline_Always (Get_Controller);
-
-   --  Grant the permission to access to the given <b>URI</b> to users having the <b>To</b>
-   --  permissions.
---     procedure Grant_URI_Permission (Manager : in out Permission_Manager;
---                                     URI     : in String;
---                                     To      : in String);
-
-   --  Grant the permission to access to the given <b>Path</b> to users having the <b>To</b>
-   --  permissions.
---     procedure Grant_File_Permission (Manager : in out Permission_Manager;
---                                      Path    : in String;
---                                      To      : in String);
 
    --  Read the policy file
    procedure Read_Policy (Manager : in out Policy_Manager;
@@ -166,66 +131,17 @@ private
 
    use Util.Strings;
 
+   subtype Permission_Index is Permissions.Permission_Index;
+
    type Permission_Type_Array is array (1 .. 10) of Permission_Type;
 
-   type Permission_Index_Array is array (Positive range <>) of Permission_Index;
-
-   --  The <b>Access_Rule</b> represents a list of permissions to verify to grant
-   --  access to the resource.  To make it simple, the user must have one of the
-   --  permission from the list.  Each permission will refer to a specific permission
-   --  controller.
-   type Access_Rule (Count : Natural) is new Util.Refs.Ref_Entity with record
-      Permissions : Permission_Index_Array (1 .. Count);
-   end record;
-   type Access_Rule_Access is access all Access_Rule;
-
-   package Access_Rule_Refs is
-     new Util.Refs.Indefinite_References (Element_Type   => Access_Rule,
-                                          Element_Access => Access_Rule_Access);
-   subtype Access_Rule_Ref is Access_Rule_Refs.Ref;
-
-   --  No rule
-   --     No_Rule : constant Access_Rule := (Count => 0,
-   --                                        Permissions => (others => Permission_Index'First));
-
-   --  Find the access rule of the policy that matches the given URI.
-   --  Returns the No_Rule value (disable access) if no rule is found.
-   function Find_Access_Rule (Manager : in Permission_Manager;
-                              URI     : in String) return Access_Rule_Ref;
-
-   --  The <b>Policy</b> defines the access rules that are applied on a given
-   --  URL, set of URLs or files.
-   type Policy is record
-      Id      : Natural;
-      Pattern : GNAT.Regexp.Regexp;
-      Rule    : Access_Rule_Ref;
-   end record;
-
-   --  The <b>Policy_Vector</b> represents the whole permission policy.  The order of
-   --  policy in the list is important as policies can override each other.
-   package Policy_Vector is new Ada.Containers.Vectors (Index_Type   => Positive,
-                                                        Element_Type => Policy);
-
-   package Rules_Maps is new Ada.Containers.Hashed_Maps (Key_Type        => String_Ref,
-                                                         Element_Type    => Access_Rule_Ref,
-                                                         Hash            => Hash,
-                                                         Equivalent_Keys => Equivalent_Keys,
-                                                         "="             => Access_Rule_Refs."=");
-
-   type Rules is new Util.Refs.Ref_Entity with record
-      Map : Rules_Maps.Map;
-   end record;
-   type Rules_Access is access all Rules;
-
-   package Rules_Ref is new Util.Refs.References (Rules, Rules_Access);
-
-   type Rules_Ref_Access is access Rules_Ref.Atomic_Ref;
+   type Permission_Index_Array is array (Positive range <>) of Permissions.Permission_Index;
 
    type Controller_Access_Array_Access is access all Controller_Access_Array;
 
    type Policy_Manager is new Ada.Finalization.Limited_Controlled with record
-      Cache        : Rules_Ref_Access;
-      Policies     : Policy_Vector.Vector;
+--        Cache        : Rules_Ref_Access;
+--        Policies     : Policy_Vector.Vector;
       Permissions  : Controller_Access_Array_Access;
       Last_Index   : Permission_Index := Permission_Index'First;
    end record;
