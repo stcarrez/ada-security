49c20de4e99746bcd66a3c6011756b6539e9b355
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-11-21 21:47:11 +0000

    Implement the URL security policy from the old Security.Permissions package

---
+++ src/security-policies-urls.adb
@@ -0,0 +1,276 @@
+-----------------------------------------------------------------------
+--  security-permissions -- Definition of permissions
+--  Copyright (C) 2010, 2011, 2012 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Ada.Containers.Hashed_Maps;
+with Ada.Containers.Vectors;
+with Ada.Unchecked_Deallocation;
+with Ada.Strings.Unbounded;
+
+with Util.Refs;
+with Util.Beans.Objects;
+with Util.Beans.Objects.Vectors;
+with Util.Serialize.IO.XML;
+with Util.Serialize.Mappers;
+with Util.Serialize.Mappers.Record_Mapper;
+
+with GNAT.Regexp;
+with Security.Controllers;
+
+package body Security.Policies.Urls is
+
+   --  ------------------------------
+   --  URL policy
+   --  ------------------------------
+
+   --  Get the policy name.
+   overriding
+   function Get_Name (From : in URL_Policy) return String is
+   begin
+      return NAME;
+   end Get_Name;
+
+   --  Returns True if the user has the permission to access the given URI permission.
+   function Has_Permission (Manager    : in URL_Policy;
+                            Context    : in Security_Context_Access;
+                            Permission : in URI_Permission'Class) return Boolean is
+      Name  : constant String_Ref := To_String_Ref (Permission.URI);
+      Ref   : constant Rules_Ref.Ref := Manager.Cache.Get;
+      Rules : constant Rules_Access := Ref.Value;
+      Pos   : constant Rules_Maps.Cursor := Rules.Map.Find (Name);
+      Rule  : Access_Rule_Ref;
+   begin
+      --  If the rule is not in the cache, search for the access rule that
+      --  matches our URI.  Update the cache.  This cache update is thread-safe
+      --  as the cache map is never modified: a new cache map is installed.
+      if not Rules_Maps.Has_Element (Pos) then
+         declare
+            New_Ref : constant Rules_Ref.Ref := Rules_Ref.Create;
+         begin
+            Rule := Manager.Find_Access_Rule (Permission.URI);
+            New_Ref.Value.all.Map := Rules.Map;
+            New_Ref.Value.all.Map.Insert (Name, Rule);
+            Manager.Cache.Set (New_Ref);
+         end;
+      else
+         Rule := Rules_Maps.Element (Pos);
+      end if;
+
+      --  Check if the user has one of the required permission.
+      declare
+         P       : constant Access_Rule_Access := Rule.Value;
+         Granted : Boolean;
+      begin
+         if P /= null then
+            for I in P.Permissions'Range loop
+--                 Context.Has_Permission (P.Permissions (I), Granted);
+               if Granted then
+                  return True;
+               end if;
+            end loop;
+         end if;
+      end;
+      return False;
+   end Has_Permission;
+
+   --  Grant the permission to access to the given <b>URI</b> to users having the <b>To</b>
+   --  permissions.
+   procedure Grant_URI_Permission (Manager : in out URL_Policy;
+                                   URI     : in String;
+                                   To      : in String) is
+   begin
+      null;
+   end Grant_URI_Permission;
+
+   --  ------------------------------
+   --  Policy Configuration
+   --  ------------------------------
+   type Policy_Config is record
+      Id          : Natural := 0;
+      Permissions : Util.Beans.Objects.Vectors.Vector;
+      Patterns    : Util.Beans.Objects.Vectors.Vector;
+      Manager     : URL_Policy_Access;
+   end record;
+   type Policy_Config_Access is access all Policy_Config;
+
+   --  Setup the XML parser to read the <b>policy</b> description.  For example:
+
+   --  Find the access rule of the policy that matches the given URI.
+   --  Returns the No_Rule value (disable access) if no rule is found.
+   function Find_Access_Rule (Manager : in URL_Policy;
+                              URI     : in String) return Access_Rule_Ref is
+
+      Matched : Boolean := False;
+      Result  : Access_Rule_Ref;
+
+      procedure Match (P : in Policy);
+
+      procedure Match (P : in Policy) is
+      begin
+         if GNAT.Regexp.Match (URI, P.Pattern) then
+            Matched := True;
+            Result  := P.Rule;
+         end if;
+      end Match;
+
+      Last : constant Natural := Manager.Policies.Last_Index;
+   begin
+      for I in 1 .. Last loop
+         Manager.Policies.Query_Element (I, Match'Access);
+         if Matched then
+            return Result;
+         end if;
+      end loop;
+      return Result;
+   end Find_Access_Rule;
+
+   --  ------------------------------
+   --  Initialize the permission manager.
+   --  ------------------------------
+   overriding
+   procedure Initialize (Manager : in out URL_Policy) is
+   begin
+      Manager.Cache := new Rules_Ref.Atomic_Ref;
+      Manager.Cache.Set (Rules_Ref.Create);
+   end Initialize;
+
+   --  ------------------------------
+   --  Finalize the permission manager.
+   --  ------------------------------
+   overriding
+   procedure Finalize (Manager : in out URL_Policy) is
+      use Ada.Strings.Unbounded;
+      use Security.Controllers;
+
+      procedure Free is
+        new Ada.Unchecked_Deallocation (Rules_Ref.Atomic_Ref,
+                                        Rules_Ref_Access);
+      procedure Free is
+        new Ada.Unchecked_Deallocation (Security.Controllers.Controller'Class,
+                                        Security.Controllers.Controller_Access);
+      procedure Free is
+        new Ada.Unchecked_Deallocation (Controller_Access_Array,
+                                        Controller_Access_Array_Access);
+
+   begin
+      Free (Manager.Cache);
+--        for I in Manager.Names'Range loop
+--           exit when Manager.Names (I) = null;
+--           Ada.Strings.Unbounded.Free (Manager.Names (I));
+--        end loop;
+
+   end Finalize;
+
+   type Policy_Fields is (FIELD_ID, FIELD_PERMISSION, FIELD_URL_PATTERN, FIELD_POLICY);
+
+   procedure Set_Member (P     : in out Policy_Config;
+                         Field : in Policy_Fields;
+                         Value : in Util.Beans.Objects.Object);
+
+   procedure Process (Policy : in Policy_Config);
+
+   procedure Set_Member (P     : in out Policy_Config;
+                         Field : in Policy_Fields;
+                         Value : in Util.Beans.Objects.Object) is
+   begin
+      case Field is
+         when FIELD_ID =>
+            P.Id := Util.Beans.Objects.To_Integer (Value);
+
+         when FIELD_PERMISSION =>
+            P.Permissions.Append (Value);
+
+         when FIELD_URL_PATTERN =>
+            P.Patterns.Append (Value);
+
+         when FIELD_POLICY =>
+            Process (P);
+            P.Id := 0;
+            P.Permissions.Clear;
+            P.Patterns.Clear;
+
+      end case;
+   end Set_Member;
+
+   procedure Process (Policy : in Policy_Config) is
+      Pol    : Security.Policies.URLs.Policy;
+      Count  : constant Natural := Natural (Policy.Permissions.Length);
+      Rule   : constant Access_Rule_Ref := Access_Rule_Refs.Create (new Access_Rule (Count));
+      Iter   : Util.Beans.Objects.Vectors.Cursor := Policy.Permissions.First;
+      Pos    : Positive := 1;
+   begin
+      Pol.Rule := Rule;
+
+      --  Step 1: Initialize the list of permission index in Access_Rule from the permission names.
+      while Util.Beans.Objects.Vectors.Has_Element (Iter) loop
+         declare
+            Perm : constant Util.Beans.Objects.Object := Util.Beans.Objects.Vectors.Element (Iter);
+            Name : constant String := Util.Beans.Objects.To_String (Perm);
+         begin
+            Rule.Value.all.Permissions (Pos) := Permissions.Get_Permission_Index (Name);
+            Pos := Pos + 1;
+
+         exception
+            when Invalid_Name =>
+               raise Util.Serialize.Mappers.Field_Error with "Invalid permission: " & Name;
+         end;
+         Util.Beans.Objects.Vectors.Next (Iter);
+      end loop;
+
+      --  Step 2: Create one policy for each URL pattern
+      Iter := Policy.Patterns.First;
+      while Util.Beans.Objects.Vectors.Has_Element (Iter) loop
+         declare
+            Pattern : constant Util.Beans.Objects.Object
+              := Util.Beans.Objects.Vectors.Element (Iter);
+         begin
+            Pol.Id   := Policy.Id;
+            Pol.Pattern := GNAT.Regexp.Compile (Util.Beans.Objects.To_String (Pattern));
+            Policy.Manager.Policies.Append (Pol);
+         end;
+         Util.Beans.Objects.Vectors.Next (Iter);
+      end loop;
+   end Process;
+
+   package Policy_Mapper is
+     new Util.Serialize.Mappers.Record_Mapper (Element_Type        => Policy_Config,
+                                               Element_Type_Access => Policy_Config_Access,
+                                               Fields              => Policy_Fields,
+                                               Set_Member          => Set_Member);
+
+   Policy_Mapping        : aliased Policy_Mapper.Mapper;
+
+   --  Setup the XML parser to read the <b>policy</b> description.
+   overriding
+   procedure Set_Reader_Config (Policy : in out URL_Policy;
+                                Reader : in out Util.Serialize.IO.XML.Parser) is
+
+--        Policy_Mapping        : aliased Policy_Mapper.Mapper;
+      Config : Policy_Config_Access := new Policy_Config;
+   begin
+      Reader.Add_Mapping ("policy-rules", Policy_Mapping'Access);
+      Reader.Add_Mapping ("module", Policy_Mapping'Access);
+      Config.Manager := Policy'Unchecked_Access;
+      Policy_Mapper.Set_Context (Reader, Config);
+   end Set_Reader_Config;
+
+begin
+   Policy_Mapping.Add_Mapping ("policy", FIELD_POLICY);
+   Policy_Mapping.Add_Mapping ("policy/@id", FIELD_ID);
+   Policy_Mapping.Add_Mapping ("policy/permission", FIELD_PERMISSION);
+   Policy_Mapping.Add_Mapping ("policy/url-pattern", FIELD_URL_PATTERN);
+end Security.Policies.Urls;
