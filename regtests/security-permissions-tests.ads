ed72ef2ff31f2df47154ccbc2967c088ba985596
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-11-22 20:18:48 +0000

    Remove the policy specific unit tests

--- regtests/security-permissions-tests.ads
+++ regtests/security-permissions-tests.ads
@@ -27,35 +27,4 @@ package Security.Permissions.Tests is
    --  Test Add_Permission and Get_Permission_Index
    procedure Test_Add_Permission (T : in out Test);
 
-   --  Test Create_Role and Get_Role_Name
-   procedure Test_Create_Role (T : in out Test);
-
-   --  Test Has_Permission
-   procedure Test_Has_Permission (T : in out Test);
-
-   --  Test reading policy files
-   procedure Test_Read_Policy (T : in out Test);
-
-   --  Test reading policy files and using the <role-permission> controller
-   procedure Test_Role_Policy (T : in out Test);
-
-      --  Read the policy file <b>File</b> and perform a test on the given URI
-   --  with a user having the given role.
-   procedure Check_Policy (T     : in out Test;
-                           File  : in String;
-                           Role  : in String;
-                           URI   : in String);
-
-   type Test_Principal is new Principal with record
-      Name  : Util.Strings.String_Ref;
-      Roles : Role_Map := (others => False);
-   end record;
-
-   --  Returns true if the given permission is stored in the user principal.
-   function Has_Role (User : in Test_Principal;
-                      Role : in Role_Type) return Boolean;
-
-   --  Get the principal name.
-   function Get_Name (From : in Test_Principal) return String;
-
 end Security.Permissions.Tests;
