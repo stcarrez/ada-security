b7284564ed543d63b6d6135ca9c1acf5103abbf5
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-11-30 21:34:56 +0000

    Check if the principal implements the Role_Principal_Context interface and get the user's roles with it

--- src/security-controllers-roles.adb
+++ src/security-controllers-roles.adb
@@ -26,15 +26,25 @@ package body Security.Controllers.Roles is
                             Context    : in Security.Contexts.Security_Context'Class;
                             Permission : in Security.Permissions.Permission'Class)
                             return Boolean is
+      pragma Unreferenced (Permission);
+
       use type Security.Principal_Access;
 
-      P : constant Security.Principal_Access := Context.Get_User_Principal;
+      P     : constant Security.Principal_Access := Context.Get_User_Principal;
+      Roles : Security.Policies.Roles.Role_Map;
    begin
       if P /= null then
+         --  If the principal has some roles, get them.
+         if P.all in Policies.Roles.Role_Principal_Context'Class then
+            Roles := Policies.Roles.Role_Principal_Context'Class (P.all).Get_Roles;
+         else
+            return False;
+         end if;
+
          for I in Handler.Roles'Range loop
---              if P.Has_Role (Handler.Roles (I)) then
+            if Roles (Handler.Roles (I)) then
                return True;
---              end if;
+            end if;
          end loop;
       end if;
       return False;
