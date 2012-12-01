67f0689bebf49147e8ae5c6d8d30347a282037b3
Stephane Carrez <Stephane.Carrez@gmail.com>
2012-12-01 15:43:52 +0000

    Implement the new unit tests for Permissions.Definition package

--- regtests/security-permissions-tests.adb
+++ regtests/security-permissions-tests.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  Security-permissions-tests - Unit tests for Security.Permissions
---  Copyright (C) 2011 Stephane Carrez
+--  Copyright (C) 2011, 2012 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -34,6 +34,10 @@ package body Security.Permissions.Tests is
                        Test_Add_Permission'Access);
       Caller.Add_Test (Suite, "Test Security.Permissions.Get_Permission_Index",
                        Test_Add_Permission'Access);
+      Caller.Add_Test (Suite, "Test Security.Permissions.Definition",
+                       Test_Define_Permission'Access);
+      Caller.Add_Test (Suite, "Test Security.Permissions.Get_Permission_Index (invalid name)",
+                       Test_Get_Invalid_Permission'Access);
    end Add_Tests;
 
    --  ------------------------------
@@ -53,5 +57,46 @@ package body Security.Permissions.Tests is
                 "Add_Permission failed");
 
    end Test_Add_Permission;
+
+   --  ------------------------------
+   --  Test the permission created by the Definition package.
+   --  ------------------------------
+   procedure Test_Define_Permission (T : in out Test) is
+      Index : Permission_Index;
+   begin
+      Index := Get_Permission_Index ("admin");
+      T.Assert (P_Admin.Permission = Index, "Invalid permission for admin");
+
+      Index := Get_Permission_Index ("create");
+      T.Assert (P_Create.Permission = Index, "Invalid permission for create");
+
+      Index := Get_Permission_Index ("update");
+      T.Assert (P_Update.Permission = Index, "Invalid permission for update");
+
+      Index := Get_Permission_Index ("delete");
+      T.Assert (P_Delete.Permission = Index, "Invalid permission for delete");
+
+      T.Assert (P_Admin.Permission /= P_Create.Permission, "Admin or create permission invalid");
+      T.Assert (P_Admin.Permission /= P_Update.Permission, "Admin or update permission invalid");
+      T.Assert (P_Admin.Permission /= P_Delete.Permission, "Admin or delete permission invalid");
+      T.Assert (P_Create.Permission /= P_Update.Permission, "Create or update permission invalid");
+      T.Assert (P_Create.Permission /= P_Delete.Permission, "Create or delete permission invalid");
+      T.Assert (P_Update.Permission /= P_Delete.Permission, "Create or delete permission invalid");
+   end Test_Define_Permission;
+
+   --  ------------------------------
+   --  Test Get_Permission on invalid permission name.
+   --  ------------------------------
+   procedure Test_Get_Invalid_Permission (T : in out Test) is
+      Index : Permission_Index;
+   begin
+      Index := Get_Permission_Index ("invalid");
+      T.Assert (Index = Index - 1,
+                "No exception raised by Get_Permission_Index for an invalid name");
+
+   exception
+      when Invalid_Name =>
+         null;
+   end Test_Get_Invalid_Permission;
 
 end Security.Permissions.Tests;
