a26c8182ce15341b8b5f9aae28e38c4607c25899
Stephane Carrez <Stephane.Carrez@gmail.com>
2013-05-12 13:14:24 +0000

    Document the OpenID configuration parameters

--- src/security-auth-openid.ads
+++ src/security-auth-openid.ads
@@ -16,92 +16,11 @@
 --  limitations under the License.
 -----------------------------------------------------------------------
 
---  == OpenID ==
---  The <b>Security.OpenID</b> package implements an authentication framework based
---  on OpenID 2.0.
+--  === OpenID Configuration ==
+--  The Open ID provider needs the following configuration parameters:
 --
---  See OpenID Authentication 2.0 - Final
---  http://openid.net/specs/openid-authentication-2_0.html
---
---  There are basically two steps that an application must implement:
---
---    * <b>Discovery</b>: to resolve and use the OpenID provider and redirect the user to the
---      provider authentication form.
---    * <b>Verify</b>: to decode the authentication and check its result.
---
---  [http://ada-security.googlecode.com/svn/wiki/OpenID.png]
---
---  The authentication process is the following:
---
---    * The application should redirect the user to the authentication URL.
---    * The OpenID provider authenticate the user and redirects the user to the callback CB.
---    * The association is decoded from the callback parameter.
---    * The <b>Verify</b> procedure is called with the association to check the result and
---      obtain the authentication results.
---
---  === Initialization ===
---  The initialization process must be done before each two steps (discovery and verify).
---  The OpenID manager must be declared and configured.
---
---    Mgr   : Security.OpenID.Manager;
---
---  For the configuration, the <b>Initialize</b> procedure is called to configure
---  the OpenID realm and set the OpenID return callback URL.  The return callback
---  must be a valid URL that is based on the realm.  Example:
---
---    Mgr.Initialize (Name      => "http://app.site.com/auth",
---                    Return_To => "http://app.site.com/auth/verify");
---
---  After this initialization, the OpenID manager can be used in the authentication process.
---
---  === Discovery: creating the authentication URL ===
---  The first step is to create an authentication URL to which the user must be redirected.
---  In this step, we have to create an OpenID manager, discover the OpenID provider,
---  do the association and get an <b>End_Point</b>.  The OpenID provider is specified as an
---  URL, below is an example for Google OpenID:
---
---    Provider : constant String := "https://www.google.com/accounts/o8/id";
---    OP       : Security.OpenID.End_Point;
---    Assoc    : constant Security.OpenID.Association_Access := new Security.OpenID.Association;
---
---  The following steps are performed:
---
---    * The <b>Discover</b> procedure is called to retrieve from the OpenID provider the XRDS
---      stream and identify the provider.  An <b>End_Point</b> is returned in <tt>OP</tt>.
---    * The <b>Associate</b> procedure is called to make the association with the <b>End_Point</b>.
---      The <b>Association</b> record holds session, and authentication.
---
---    Mgr.Discover (Provider, OP);  --  Yadis discovery (get the XRDS file).
---    Mgr.Associate (OP, Assoc.all);--  Associate and get an end-point with a key.
---
---  After this first step, you must manage to save the association in the HTTP session.
---  Then you must redirect to the authentication URL that is obtained by using:
---
---    Auth_URL : constant String := Mgr.Get_Authentication_URL (OP, Assoc.all);
---
---  === Verify: acknowledge the authentication in the callback URL ===
---  The second step is done when the user has finished the authentication successfully or not.
---  For this step, the application must get back the association that was saved in the session.
---  It must also prepare a parameters object that allows the OpenID framework to get the
---  URI parameters from the return callback.
---
---    Assoc   : Association_Access := ...;  --  Get the association saved in the session.
---    Auth    : OpenID.Authentication;
---    Params  : Auth_Params;
---
---  The OpenID manager must be initialized and the <b>Verify</b> procedure is called with
---  the association, parameters and the authentication result.  The <b>Get_Status</b> function
---  must be used to check that the authentication succeeded.
---
---    Mgr.Verify (Assoc.all, Params, Auth);
---    if Security.OpenID.Get_Status (Auth) = Security.OpenID.AUTHENTICATED then ...  -- Success.
---
---  === Principal creation ===
---  After the user is successfully authenticated, a user principal can be created and saved in
---  the session.  The user principal can then be used to assign permissions to that user and
---  enforce the application permissions using the security policy manger.
---
---    P : Security.OpenID.Principal_Access := Security.OpenID.Create_Principal (Auth);
+--    openid.realm          The OpenID realm parameter passed in the authentication URL.
+--    openid.callback_url   The OpenID return_to parameter.
 --
 private package Security.Auth.OpenID is
 
@@ -111,7 +30,8 @@ private package Security.Auth.OpenID is
    --  The <b>Manager</b> provides the core operations for the OpenID process.
    type Manager is new Security.Auth.Manager with private;
 
-   --  Initialize the authentication realm.
+   --  Initialize the OpenID authentication realm.  Get the <tt>openid.realm</tt>
+   --  and <tt>openid.callback_url</tt> parameters to configure the realm.
    overriding
    procedure Initialize (Realm     : in out Manager;
                          Params    : in Parameters'Class;
@@ -135,6 +55,8 @@ private package Security.Auth.OpenID is
                         OP     : in End_Point;
                         Result : out Association);
 
+   --  Get the authentication URL to which the user must be redirected for authentication
+   --  by the authentication server.
    overriding
    function Get_Authentication_URL (Realm : in Manager;
                                     OP    : in End_Point;
