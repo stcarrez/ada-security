8d27c313fa1afe83901d2699f6a3ad5ab4df66e7
Stephane Carrez <stcarrez@bouyguestelecom.fr>
2017-02-16 23:55:19 +0100

    Register the new test to execute it

--- regtests/security-testsuite.adb
+++ regtests/security-testsuite.adb
@@ -1,6 +1,6 @@
 -----------------------------------------------------------------------
 --  Security testsuite - Ada Security Test suite
---  Copyright (C) 2011, 2012, 2013 Stephane Carrez
+--  Copyright (C) 2011, 2012, 2013, 2017 Stephane Carrez
 --  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
 --
 --  Licensed under the Apache License, Version 2.0 (the "License");
@@ -20,6 +20,7 @@ with Security.Permissions.Tests;
 with Security.Policies.Tests;
 with Security.OAuth.JWT.Tests;
 with Security.OAuth.Clients.Tests;
+with Security.Random.Tests;
 package body Security.Testsuite is
 
    Tests : aliased Util.Tests.Test_Suite;
@@ -27,6 +28,7 @@ package body Security.Testsuite is
    function Suite return Util.Tests.Access_Test_Suite is
       Ret : constant Util.Tests.Access_Test_Suite := Tests'Access;
    begin
+      Security.Random.Tests.Add_Tests (Ret);
       Security.OAuth.JWT.Tests.Add_Tests (Ret);
       Security.Auth.Tests.Add_Tests (Ret);
       Security.Permissions.Tests.Add_Tests (Ret);
