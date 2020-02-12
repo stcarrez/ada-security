3b14917bb0ff0d323f6c259d8bf48b75dcc814b2
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-02-12 23:28:08 +0100

    Update and cleanup coverage script

--- coverage.sh
+++ coverage.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
-lcov --base-directory . --directory . -c -o security.cov
-bin/security_harness -xml security-aunit.xml
-lcov --base-directory . --directory . -c -o security.cov
-lcov --remove security.cov "/usr*" -o security.cov
-lcov --remove security.cov "regtests*" -o security.cov
-lcov --remove security.cov "ada-util/*" -o security.cov
-lcov --remove security.cov ada-security/b__security_harness.adb -o security.cov
+NAME=security.cov
+lcov --base-directory . --directory . -c --include "*/ada-security/src/*" -o $NAME
 rm -rf cover
-genhtml -o ./cover -t "test coverage" --num-spaces 4 security.cov
+genhtml --ignore-errors source -o ./cover -t "test coverage" --num-spaces 4 $NAME
  
