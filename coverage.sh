e5a17b06fcf92b00f29295c369c5433d284b4d1f
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-02-14 23:39:54 +0100

    Add --quiet for coverage

--- coverage.sh
+++ coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 NAME=security.cov
-lcov --base-directory . --directory . -c --include "*/ada-security/src/*" -o $NAME
+lcov --quiet --base-directory . --directory . -c --include "*/ada-security/src/*" -o $NAME
 rm -rf cover
-genhtml --ignore-errors source -o ./cover -t "test coverage" --num-spaces 4 $NAME
+genhtml --quiet --ignore-errors source -o ./cover -t "test coverage" --num-spaces 4 $NAME
  
