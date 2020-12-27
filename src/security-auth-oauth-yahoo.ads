589f45d9be8e65fc933ed0cef7d2130d5e809b28
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-12-27 14:52:31 +0100

    New package to support Yahoo! OpenId connect and get the user information

---
+++ src/security-auth-oauth-yahoo.ads
@@ -0,0 +1,36 @@
+-----------------------------------------------------------------------
+--  security-auth-oauth-yahoo -- Yahoo! OAuth based authentication
+--  Copyright (C) 2013, 2014, 2020 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Security.OAuth.Clients;
+package Security.Auth.OAuth.Yahoo is
+
+   --  ------------------------------
+   --  OAuth Manager
+   --  ------------------------------
+   --  The <b>Manager</b> provides the core operations for the OAuth authorization process.
+   type Manager is new Security.Auth.OAuth.Manager with null record;
+
+   --  Verify the OAuth access token and retrieve information about the user.
+   overriding
+   procedure Verify_Access_Token (Realm   : in Manager;
+                                  Assoc   : in Association;
+                                  Request : in Parameters'Class;
+                                  Token   : in Security.OAuth.Clients.Access_Token_Access;
+                                  Result  : in out Authentication);
+
+end Security.Auth.OAuth.Yahoo;
