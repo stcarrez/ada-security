84bcd42cc042ea37e6d0fe212545fdbd01b4e1a9
Stephane Carrez <Stephane.Carrez@gmail.com>
2020-12-27 18:15:05 +0100

    Add GitHub OAuth support

---
+++ src/security-auth-oauth-github.adb
@@ -0,0 +1,68 @@
+-----------------------------------------------------------------------
+--  security-auth-oauth-github -- Github OAuth based authentication
+--  Copyright (C) 2013, 2014, 2020 Stephane Carrez
+--  Written by Stephane Carrez (Stephane.Carrez@gmail.com)
+--
+--  Licensed under the Apache License, Version 2.0 (the "License");
+--  you may not use this file except in compliance with the License.
+--  You may obtain a copy of the License at
+--
+--      http://www.apache.org/licenses/LICENSE-2.0
+--
+--  Unless required by applicable law or agreed to in writing, software
+--  distributed under the License is distributed on an "AS IS" BASIS,
+--  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--  See the License for the specific language governing permissions and
+--  limitations under the License.
+-----------------------------------------------------------------------
+
+with Util.Log.Loggers;
+with Util.Beans.Objects;
+with Util.Http.Clients;
+with Util.Properties.JSON;
+package body Security.Auth.OAuth.Github is
+
+   use Util.Log;
+
+   Log : constant Loggers.Logger := Util.Log.Loggers.Create ("Security.Auth.OAuth.Github");
+
+   --  ------------------------------
+   --  Verify the OAuth access token and retrieve information about the user.
+   --  ------------------------------
+   overriding
+   procedure Verify_Access_Token (Realm   : in Manager;
+                                  Assoc   : in Association;
+                                  Request : in Parameters'Class;
+                                  Token   : in Security.OAuth.Clients.Access_Token_Access;
+                                  Result  : in out Authentication) is
+      pragma Unreferenced (Request);
+   begin
+      --  The id_token is a JWT token that must be decoded and verified.
+      --  See https://developer.yahoo.com/oauth2/guide/openid_connect/decode_id_token.html
+      --  It contains information to identify the user.
+      declare
+         URI      : constant String := "https://api.github.com/user";
+         Http     : Util.Http.Clients.Client;
+         Reply    : Util.Http.Clients.Response;
+         Props    : Util.Properties.Manager;
+         Value    : Util.Beans.Objects.Object;
+      begin
+         Http.Add_Header ("Authorization", "token " & Token.Get_Name);
+         Http.Get (URI, Reply);
+
+         Util.Properties.JSON.Parse_JSON (Props, Reply.Get_Body);
+
+         Result.Identity := Realm.Issuer;
+         Append (Result.Identity, "/");
+         Append (Result.Identity, String '(Props.Get ("id")));
+         Result.Claimed_Id := Result.Identity;
+         Result.First_Name := To_Unbounded_String (Props.Get ("name"));
+         Result.Full_Name := To_Unbounded_String (Props.Get ("name"));
+
+         --  The email is optional and depends on the scope.
+         Result.Email := To_Unbounded_String (Props.Get ("email"));
+         Set_Result (Result, AUTHENTICATED, "authenticated");
+      end;
+   end Verify_Access_Token;
+
+end Security.Auth.OAuth.Github;
