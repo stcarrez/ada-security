ae269e65ca2ecd307131c955cc8aa42c7854cba3
Stephane Carrez <Stephane.Carrez@gmail.com>
2024-08-20 21:49:56 +0200

    Run lcov from alr to get access to the correct gcov tool

--- coverage.sh
+++ coverage.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 NAME=security.cov
-lcov --quiet --base-directory . --directory . \
+alr exec -- lcov --quiet --base-directory . --directory . \
    --no-external \
    --exclude '*/b__*.adb' \
    --exclude '*/samples/*' \
